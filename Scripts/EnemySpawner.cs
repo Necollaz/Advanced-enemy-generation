c3a584d90db4aae4e9e0b30c59ff90347925a837
Necollaz <156367647+Necollaz@users.noreply.github.com>
2024-03-07 21:53:28 +0300

    new

--- Scripts/EnemySpawner.cs
+++ Scripts/EnemySpawner.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private Transform[] _spawnPoints;
-    [SerializeField] private Rigidbody[] _enemyPrefab;
+    [SerializeField] private Rigidbody[] _enemyPrefabs;
     [SerializeField] private Transform[] _targets;
     [SerializeField] private float _timeToSpawn;
     [SerializeField] private bool _canSpawn = true;
 
     private Transform[] _spawnTarget;
+    private int[] _enemyPrefabIndex;
     private WaitForSeconds _spawnWait;
     private bool _isSpawning = false;
 
@@ -23,20 +24,22 @@ public class EnemySpawner : MonoBehaviour
     private void InitializeSpawnTargets()
     {
         _spawnTarget = new Transform[_spawnPoints.Length];
+        _enemyPrefabIndex = new int[_spawnPoints.Length];
 
         for (int i = 0; i < _spawnPoints.Length; i++)
         {
             _spawnTarget[i] = _targets[i % _targets.Length];
+            _enemyPrefabIndex[i] = i % _enemyPrefabs.Length;
         }
     }
 
     private void Spawn()
     {
         int randomSpawnIndex = Random.Range(0, _spawnPoints.Length);
-        int randomEnemyIndex = Random.Range(0, _enemyPrefab.Length);
 
         Transform spawnPoint = _spawnPoints[randomSpawnIndex];
-        Rigidbody enemyObject = Instantiate(_enemyPrefab[randomEnemyIndex]);
+        Rigidbody enemyPrefab = _enemyPrefabs[_enemyPrefabIndex[randomSpawnIndex]];
+        Rigidbody enemyObject = Instantiate(enemyPrefab);
         enemyObject.transform.position = spawnPoint.position;
         Transform target = _spawnTarget[randomSpawnIndex];
         Enemy enemyComponent = enemyObject.GetComponent<Enemy>();
